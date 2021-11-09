a94b828e89136544350520685a816a919211f939
ACarfi <carfi.alessandro@gmail.com>
2021-11-09 15:23:07 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 using namespace std;
 
+
 int main() {
-   return 0;
+    int n=0;
+    int fibonacci_p= 1, fibonacci=1;
+    cin >> n;
+    if(n>=2) {
+        cout << fibonacci << endl;
+        cout << fibonacci_p << endl;
+        for (int i = 1; i < n - 1; i++) {
+            int temp = fibonacci;
+            fibonacci += fibonacci_p;
+            fibonacci_p = temp;
+            cout << fibonacci << endl;
+        }
+    }else{
+        cout << "errore"<<endl;
+    }
+    return 0;
 }
